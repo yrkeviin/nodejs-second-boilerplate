eaeaa1bb3a06a5505ff456dfc9f997fd3398ad10
Kevin Lima <kevineziquiellima160@gmail.com>
2024-10-31 17:12:21 -0300

    Feat: Create method delete

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -40,6 +40,18 @@ class UsersRepository {
 
         return user;
     }
+
+    deleteUser(id) {
+        const user = this.getUserById(id);
+
+        if (!user) {
+            return null;
+        }
+
+        this.users = this.users.filter((u) => u.id != id);
+
+        return user;
+    }
 }
 
 export default UsersRepository;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -63,7 +63,20 @@ usuariosRoutes.put("/:id", (req, res) => {
 });
 
 usuariosRoutes.delete("/:id", (req, res) => {
-    
+    const { id } = req.params;
+
+    const user = usersList.deleteUser(id);
+
+    if (!user) {
+        return res.status(404).json({
+            message: `Usuário com id ${id} não encontrado!`,
+        });
+    }
+
+    return res.status(200).json({
+        message: `Usuário com id ${id} foi deletado!`,
+        user,
+    });
 });
 
 export default usuariosRoutes;
