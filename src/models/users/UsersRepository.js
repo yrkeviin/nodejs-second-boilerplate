6a2705a4e13999c7504a074d0cb02e0926ddb036
Kevin Lima <kevineziquiellima160@gmail.com>
2024-10-31 16:47:25 -0300

    Feat: Create get user bby id method

--- src/models/users/UsersRepository.js
+++ src/models/users/UsersRepository.js
@@ -16,6 +16,16 @@ class UsersRepository {
 
         return newUser;
     }
+
+    getUserById(id) {
+        const user = this.users.find((u) => u.id == id);
+
+        if (!user) {
+            return null;
+        }
+
+        return user;
+    }
 }
 
 export default UsersRepository;
--- src/routes/usuarios.routes.js
+++ src/routes/usuarios.routes.js
@@ -27,4 +27,29 @@ usuariosRoutes.post("/", (req, res) => {
     });
 });
 
+usuariosRoutes.get("/:id", (req, res) => {
+    const { id } = req.params;
+    
+    const user = usersList.getUserById(id);
+
+    if (!user) {
+        return res.status(404).json({
+            message: `Usuário com id ${id} não encontrado!`,
+        });
+    }
+
+    return res.status(200).json({
+        message: `Usuário com id ${id} foi encontrado!`,
+        user,
+    });
+});
+
+usuariosRoutes.put("/:id", (req, res) => {
+    
+});
+
+usuariosRoutes.delete("/:id", (req, res) => {
+    
+});
+
 export default usuariosRoutes;
